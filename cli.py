3e1e738f6b74562534ea49caabfc9c3f9d8251eb
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-24 00:02:01 -0500

    Change user interface

--- cli.py
+++ cli.py
@@ -3,8 +3,19 @@
 import typer
 from zippyshare_downloader import extract_info
 import enquiries
+from rich.console import Console
+from rich.theme import Theme
 
 from servers import Jkanime
+
+
+custom_theme = Theme({
+    'good': 'bold green',
+    'bad': 'bold red',
+    'warning': 'bold yellow'
+})
+
+console = Console(theme=custom_theme)
 
 
 def select_anime(anime_dict):
@@ -20,11 +31,14 @@ def select_ep(anime):
     Asks you to select one episode
     """
     ep_num = Jkanime().get_ep_num(anime['slug'])
-    print(f'{anime["title"]}')
     while True:
         try:
             ep = int(input(f'Elige un episodio ({anime["ep"]}): '))
         except Exception as e:
+            # Delete previous line
+            CURSOR_UP_ONE = '\x1b[1A'
+            ERASE_LINE = '\x1b[2K'
+            print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
             continue
         else:
             break
@@ -36,16 +50,13 @@ def select_link(links):
     Prints links and ask you to select one.
     """
     if not links:
-        print('No hay un enlace de descarga disponible')
+        console.print('No hay un enlace de descarga disponible', style="bad")
         exit()
     elif len(links) == 1:
         return links[0]
     elif len(links) > 1:
-        return enquiries.choose(
-            ('Selecciona un enlace ' +
-             '(Solo Zippyshare esta disponible para descarga)'),
-            links
-        )
+        console.print('[bold]Selecciona un enlace |[/] [warning]Solo Zippyshare esta disponible para descarga[/]')
+        return enquiries.choose('', links)
 
 
 def download(link):
@@ -53,8 +64,7 @@ def download(link):
     Download the links. Print link if not available.
     """
     if 'zippyshare' not in link:
-        print('El enlace no es de Zippyshare. Descargalo en tu navegador')
-        print(link)
+        console.print(f'Descargalo en tu navegador => {link}', style="good")
     else:
         if enquiries.confirm('¿Quieres descargar el episodio?'):
             try:
@@ -78,7 +88,14 @@ def main(
         anime = input('Buscar Anime: ')
 
     anime_dict = Jkanime().search_anime(anime)
-    anime_info = select_anime(anime_dict)
+
+    if len(anime_dict) == 1:
+        anime_info = list(anime_dict.values())[0]
+    else:
+        anime_info = select_anime(anime_dict)
+
+    # Print anime title
+    console.print(f'{anime_info["title"]}', style="good")
 
     # Select episode
     if episode is None:
